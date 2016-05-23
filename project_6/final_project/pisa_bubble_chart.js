d806265952bdec5e11297bd7a45ffea2e2744308
DingChiLin <gn01168178@yahoo.com.tw>
2016-05-23 23:01:13 +0800

    finish country highlight

--- project_6/final_project/pisa_bubble_chart.js
+++ project_6/final_project/pisa_bubble_chart.js
@@ -1,9 +1,11 @@
 function Chart(){
 
-  var svg = dimple.newSvg("#chartContainer", 1000, 800);
+  var myChart;
 
   this.init = function(data){
-    var mathData = [];
+
+    var svg = dimple.newSvg("#chartContainer", '100%', '100%');
+    var mathData = []
     var languageData = [];
     var scienceData = [];
 
@@ -19,24 +21,43 @@ function Chart(){
         splitData[s_index][index]['LearningTime'] = data[index][ subjectList[s_index] + 'LearningTime']
         splitData[s_index][index]['Score'] = data[index][ subjectList[s_index] + 'Score']
         splitData[s_index][index]['Size'] = 10
+
+        if(s_index==0){ // set the selecor option only once
+          $('#highlightCountrySelector .selectpicker').append($('<option>', {
+            value: index,
+            text: data[index]['Country']
+          }))
+        }
       }
     }
 
+    $('.selectpicker').selectpicker('refresh');
+
+    // Title
     svg.append("text")
-       .attr("x", 460)
-       .attr("y", 64)
-       .attr("text-anchor", "middle")
-       .style("font-size", "20px")
+       .attr("x", 60)
+       .attr("y", 70)
+       .attr("text-anchor", "left")
+       .style("font-size", "25px")
        .style("font-weight", "bold")
        .text("Awesome charting from Dimple.js")
 
-    var myChart = new dimple.chart(svg)
-    myChart.setBounds(60, 100, 800, 500)
+    // ICT Resource Legend
+    svg.append("text")
+       .attr("x", 600)
+       .attr("y", 70)
+       .attr("text-anchor", "left")
+       .style("font-size", "15px")
+       .style("font-weight", "bold")
+       .text("ICT Resource (A highest ~ E lowest)")
+
+
+    myChart = new dimple.chart(svg)
+    myChart.setBounds(60, 130, 800, 500)
 
     var x = myChart.addMeasureAxis("x", "Score")
-    x.overrideMin = 350
+    x.overrideMin = 360
     x.overrideMax = 600
-    console.log(x.totle)
     x.title = 'Score (plausible value)'
 
     //上課時數
@@ -48,66 +69,80 @@ function Chart(){
     //控制大小
     var z = myChart.addMeasureAxis("z", "Size")
     z.overrideMin = 5
-    z.overrideMax = 40
+    z.overrideMax = 30
 
     //['國家', ‘科技使用程度’]
     var s = myChart.addSeries(["Country", "ICTResourcesClass"], dimple.plot.bubble)
     s.stacked = false
-    s.addOrderRule(['ICTResourcesClass'])
+    s.addOrderRule(['ICTResourcesClass'], true)
 
-    myChart.addLegend(900, 100, 50, 100, "right")
+    myChart.addLegend(560, 90, 300, 20, "right")
 
     /**
      * Update Data and Draw
      */
 
-    update(myChart, splitData[0])
-
-    setTimeout(function(){
-      //update(myChart, splitData[2])
-    }, 4000);
-
+    updateData(splitData[0], true)
+    updateHighlightCountry('chinese-taipei', 'rgb(197, 52, 48)')
   }
 
-  function update(myChart, data, highlight_countries, is_ict){
+  this.changeSubject = function(index){
+    updateData(splitData[index])
+  }
+
+  this.changeHighlightCountry = function(id, color){
+    var country = $("#" + id + " option:selected").text()
+    updateHighlightCountry(parseCountryName(country), color)
+  }
+
+  function parseCountryName(name){
+    return name.toLowerCase().replace(/\(|\)|\ /g, "-")
+  }
+
+  function updateData(data, is_ict){
     myChart.data = data
+    updateICTColor(is_ict)
+    myChart.draw(1000)
+  }
 
-    // highlight choosed country (red, yellow, green)
+  function updateHighlightCountry(country, color){
     myChart.series[0].afterDraw = function (s, d) {
-      //console.log(s)
-      //console.log(d)
+      if(s.style.stroke == color && s.id.indexOf(country) == -1){
+        d3.select(s)
+          .style('stroke', s.style.fill)
+      }
 
-      d3.select('#dimple-chinese-taipei-b----')
-      .style('stroke', 'red')
-      .attr('stroke-width', '2px')
+      if(s.id.indexOf(country) > -1){
+        d3.select(s)
+          .style('stroke', color)
+      }
+
+      d3.select(s)
+        .style('stroke-width', '3px')
     }
-
-    // add color for different ICT Resource Class
-    myChart.assignColor("A", "#063870", "#063870", 1);
-    myChart.assignColor("B", "#0D51B0", "#0D51B0", 1);
-    myChart.assignColor("C", "#3188E8", "#3188E8", 1);
-    myChart.assignColor("D", "#6CB0F8", "#6CB0F8", 1);
-    myChart.assignColor("E", "#B3D9FD", "#B3D9FD", 1);
 
     myChart.draw(1000)
   }
 
-  function changeStyle(){
-          // 增大方塊size
-    d3.selectAll("circle.dimple-series-0")
-      .attr("r", 10)
+  function updateICTColor(is_ict){
+    if(is_ict){
+      myChart.assignColor("A", "#808080", "#808080", 0.6);
+      myChart.assignColor("B", "#808080", "#808080", 0.6);
+      myChart.assignColor("C", "#808080", "#808080", 0.6);
+      myChart.assignColor("D", "#808080", "#808080", 0.6);
+      myChart.assignColor("E", "#808080", "#808080", 0.6);
+    }else{
+      myChart.assignColor("A", "#063870", "#063870", 1);
+      myChart.assignColor("B", "#0D51B0", "#0D51B0", 1);
+      myChart.assignColor("C", "#3188E8", "#3188E8", 1);
+      myChart.assignColor("D", "#6CB0F8", "#6CB0F8", 1);
+      myChart.assignColor("E", "#B3D9FD", "#B3D9FD", 1);
+    }
 
-    // 國家 Highlight 直接改
-    //d3.select('#dimple-chinese-taipei-----')
-    //  .style('fill', 'red')
-
-    d3.select('#dimple-chinese-taipei-b----')
-      .style('stroke', 'red')
-      .attr('stroke-width', '2px')
-
-    d3.selectAll('text.dimple-axis')
-      .style("font-size", "15px")
-
-    //$(".dimple-legend").hide()
+    myChart.draw(1000)
   }
+
 }
+
+var chart = new Chart();
+
